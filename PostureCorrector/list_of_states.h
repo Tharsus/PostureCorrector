7f6eaa94ebc2a577eb553db3094998e6d8379ca0
Tharsus Andrade Proux <tharsus@poli.ufrj.br>
2018-03-14 15:00:48 -0300

    Including list of status with macros

---
+++ PostureCorrector/list_of_states.h
@@ -0,0 +1,15 @@
+#ifndef LIST_OF_STATES_H
+#define LIST_OF_STATES_H
+
+#define CORRECT_POSTURE     0
+#define LOW_HEIGHT          1
+#define TOO_CLOSE           2
+#define ROLL_RIGHT          4
+#define ROLL_LEFT           8
+
+#define START               100
+#define END                 101
+#define PAUSE               102
+#define COULD_NOT_DETECT    103
+
+#endif // LIST_OF_STATES_H
